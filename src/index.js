337b5ddf46dabe1b5dfca0b43ab3f0e1e5ff98fe
Joao_Godinho <joaoeustaquiosg@gmail.com>
2023-12-16 07:16:46 -0300

    First Commit

---
+++ src/index.js
@@ -0,0 +1,35 @@
+const express = require("express")
+const mongoose = require('mongoose')
+
+const app = express()
+app.use(express.json())
+const port = 3000
+
+const Film = mongoose.model('Film', {
+  title: String,
+  description: String,
+  image_url: String, 
+  trailer_url: String
+})
+
+app.get('/', async (req, res) => {
+  const films = await Film.find()
+  res.send(films)
+})
+
+app.post('/', async (req, res) => {
+  const film = new Film({
+    title: req.body.title,
+    description: req.body.description,
+    image_url: req.body.image_url,
+    trailer_url: req.body.trailer_url
+  })
+
+  await film.save();
+  res.send(film)
+})
+
+app.listen(port, () => {
+  mongoose.connect('mongodb+srv://joaoeustaquiosg:yvGxZCaHqtiuUOCe@starwars-api.ykqsl81.mongodb.net/?retryWrites=true&w=majority')
+  console.log(`App Running ${port}`)
+})
